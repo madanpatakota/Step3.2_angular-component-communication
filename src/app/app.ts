ef5fbbdd9f5c4560eca829afa5989d51e60dd775
Madan.patakota <madan.patakota@gmail.com>
2026-09-08 08:24:17 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+
 @Component({
-  imports: [],
   selector: 'app-root',
-  styleUrl: './app.css',
+  imports: [],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
 
+  employeeName = signal('Madan');
+  basicSalary = signal(30000);
+
+  yearlySalary = computed(() => this.basicSalary() * 12);
+
+  constructor() {
+    effect(() => {
+      console.log(
+        `Employee: ${this.employeeName()} | Salary: ₹${this.basicSalary()}`
+      );
+    });
+  }
+
+  changeEmployeeName() {
+    this.employeeName.set('Madan Reddy');
+  }
+
+  setSalary() {
+    this.basicSalary.set(50000);
+  }
+
+  increaseSalary() {
+    this.basicSalary.update(currentSalary => currentSalary + 5000);
+  }
 }
